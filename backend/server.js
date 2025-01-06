33dd8a4c9013a3af5650c87f59f705ddff36400c
mediwateyash <diwateyash2004@gmail.com>
2025-01-06 15:58:57 +0530

    backend done

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+//connection to mongo db
+const express = require('express');
+const User = require('./signup');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/signup', async(req, res) => {
+    try{
+        const user = new User(req.body);
+        const result = await user.save()
+        res.send(result);
+    }catch(error){
+        console.log("error in signin ")
+    }
+})
+
+
+app.listen(port, () => {
+    console.log("server is running on port 3000");
+});
